cbf7524c09a51a7e17050d55db828cc99f026fd7
hanwn <hanwn7721@foxmail.com>
2020-09-29 11:04:13 +0800

    complete threadpool

---
+++ scaffold/threadpool/threadpool.h
@@ -0,0 +1,31 @@
+#ifndef THREADPOOL_H_
+#define THREADPOOL_H_
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdint.h>
+typedef struct task{
+    void (*func)(void *);
+    void* arg;
+    struct task* next;
+}task_t;
+typedef struct threadpool{
+    pthread_mutex_t lock;
+    pthread_cond_t cond;
+    pthread_t* threads;
+    task_t* head;
+    int thread_count;
+    int queue_size;
+    int shutdown;
+    int started;
+}threadpool_t;
+
+static void* threadpool_free(threadpool_t*);
+static void* threadpool_work(void* arg);
+int threadpool_add(threadpool_t* ,void(*func)(void*),void* arg);
+int threadpool_destroy(threadpool_t*,int graceful);
+threadpool_t* threadpool_init(int thread_num);
+
+
+#endif
