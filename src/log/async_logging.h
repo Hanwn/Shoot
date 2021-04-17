97580081dd8609a420c498834baaa541c2758b43
hanwn <hanwn7721@foxmail.com>
2021-04-17 14:23:39 +0800

    :bulb: finish async write log module with comment

--- src/log/async_logging.h
+++ src/log/async_logging.h
@@ -1,9 +1,42 @@
 #ifndef ASYNC_LOGGING_H_
 #define ASYNC_LOGGING_H_
 
+#include "fixed_buffer.h"
+#include "Thread.h"
+#include "CountDownLatch.h"
+#include "Condition_.h"
+#include <vector>
+#include <memory>
+#include <iostream>
+#include "log2file.h"
+
 class AsyncLogging{
 public:
+    AsyncLogging(std::string _filename, int _flush_freq = 3);
+    ~AsyncLogging();
+
+public:
+    void append(const char* msg, int len);
     void start();
+    void stop();
+
+private:
+    void thread_func();
+    // DONE:small_buffer to large_buffer??
+    using buffer = FixedBuffer<large_buffer>;
+    using buffer_vector = std::vector<std::shared_ptr<buffer>> ;
+    using buffer_ptr = std::shared_ptr<buffer>;
+
+    const int flush_freq;
+    bool running;
+    std::string filename_;
+    Thread thread_;
+    MutexLock mutex_;
+    Condition cond_;
+    buffer_ptr curr_buffer_;
+    buffer_ptr next_buffer_;
+    buffer_vector buffers_;
+    CountDownLatch latch_;
 };
 
 #endif
--- src/log/utils.h
+++ src/log/utils.h
@@ -8,14 +8,14 @@ static pthread_once_t thread_run_once = PTHREAD_ONCE_INIT;
 static AsyncLogging* async_log;
 
 void once_init() {
-    async_log = new AsyncLogging();
+    async_log = new AsyncLogging("Logger_file.log");
     async_log->start();
 }
 
 
 void output(const char* msg, int len) {
     pthread_once(&thread_run_once, once_init);
-    // TODO:complement async append()
-    // async_log->append();
+    // DONE:complement async append()
+    async_log->append(msg, len);
 }
 #endif
