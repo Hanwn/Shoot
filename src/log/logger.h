b4a555bad150e90dbef14261f33e005e38d2215d
hanwn <hanwn7721@foxmail.com>
2021-04-17 14:48:24 +0800

    :bento: complete logger module

--- src/log/logger.h
+++ src/log/logger.h
@@ -1,6 +1,24 @@
 #ifndef _LOGGER_H_
 #define _LOGGER_H_
 
+#include "log_stream.h"
 
+class Logger {
+public:
+    Logger(const char* _file_name, int line);
+    ~Logger();
 
+public:
+    LogStream& stream();
+    void format_time();
+
+private:
+    // QUESTION:内部类的好处?
+    int line_;
+    LogStream stream_;
+    std::string file_name_;
+};
+
+ 
+#define LOG Logger(__FILE__, __LINE__).stream()
 #endif
