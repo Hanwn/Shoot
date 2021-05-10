a09cd3c9af12083a6b715a4fcc81691f08e5a9a9
Hanwn <hanwn7721@foxmail.com>
2021-05-10 15:57:33 +0800

    :loud_sound: add logs

--- src/net/TcpServer.cc
+++ src/net/TcpServer.cc
@@ -11,28 +11,32 @@ TCPServer::TCPServer(EventLoop* _loop, std::string _thread_name, int port)
     , port_(port)
     , listen_fd_(socket_bind_listen(port_)) 
     , accpet_channel_(new Channel(loop_, listen_fd_)) {
+
         handle_for_sigpipe();
         if (set_non_blocking(listen_fd_) < 0) {
             LOG<<"set non_blocking";
         }
+        LOG<<"new listen fd:"<<listen_fd_;
     }
 
 
 TCPServer::~TCPServer() {
-
+    //TODO: release resource
 }
 
 void TCPServer::start() {
     event_loop_pool_->start(cb_);
+    event_loop_pool_->set_thread_nums(4);
     accpet_channel_->set_read_callback(std::bind(&TCPServer::handle_new_conn, this));
     // 将当前channel加入到loop监听中
     accpet_channel_->enable_read();
 
     started_ = true;
+    LOG<<"Server is RUNNING";
 }
 
 void TCPServer::handle_new_conn() {
-    // 
+    LOG<<"new connection";
     struct sockaddr_in client_addr;
     ::memset(&client_addr, 0, sizeof(struct sockaddr_in));
     socklen_t client_addr_len = sizeof (client_addr);
